325402048dfe1fab98e0b30cd3e3f170e956d205
evfedoto <evfedoto@cisco.com>
2023-07-31 20:41:52 +0200

    df

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -27,6 +27,65 @@ export const Toolbar = component$(({ lang }: ToolbarProps) => {
           <div class="flex items-center md:order-2">
             <LangButton />
             <ThemeButton />
+            <button
+              id="dropdownDefaultButton"
+              data-dropdown-toggle="dropdown"
+              class="md:hidden hover:bg-teal-100 dark:hover:bg-teal-900 focus:outline-none focus:ring-0 focus:ring-teal-100 dark:focus:ring-teal-900 rounded-lg text-sm p-2.5"
+              type="button"
+            >
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+            <div
+              id="dropdown"
+              class="z-10 hidden bg-teal-50 divide-y divide-teal-100 rounded-lg shadow w-44 dark:bg-teal-950 border border-teal-100 dark:border-teal-900"
+            >
+              <ul
+                class="py-2 text-md space-y-2"
+                aria-labelledby="dropdownDefaultButton"
+              >
+                <li class="mx-4">
+              <a
+                href={lang ? `/movie?lang=${lang}` : `/movie`}
+                class="group transition duration-300"
+              >
+                Movies
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-teal-950 dark:bg-teal-50"></span>
+              </a>
+            </li>
+            <li class="mx-4">
+              <a
+                href={lang ? `/tv?lang=${lang}` : `/tv`}
+                class="group transition duration-300"
+              >
+                Series
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-teal-950 dark:bg-teal-50"></span>
+              </a>
+            </li>
+            <li class="mx-4">
+              <a
+                href={lang ? `/search?lang=${lang}` : `/search`}
+                class="group transition duration-300"
+              >
+                Search
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-teal-950 dark:bg-teal-50"></span>
+              </a>
+            </li>
+              </ul>
+            </div>
           </div>
 
           <ul class="hidden md:flex flex-row me-4">

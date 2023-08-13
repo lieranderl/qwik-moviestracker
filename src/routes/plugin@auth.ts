a820e3b59c7ce0a616ab90b7b24a56e4a0ec751d
evfedoto <evfedoto@cisco.com>
2023-08-13 13:52:10 +0200

    auth verified

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,4 +1,5 @@
 import { serverAuth$ } from "@builder.io/qwik-auth";
+import type { GoogleProfile } from "@auth/core/providers/google";
 import Google from "@auth/core/providers/google";
 import type { Provider } from "@auth/core/providers";
 
@@ -12,4 +13,15 @@ export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
         clientSecret: env.get("GOOGLE_SECRET")!,
       }),
     ] as Provider[],
+    callbacks: {
+      async signIn({ account, profile }) {
+        if (account && profile) {
+          if (account.provider === "google") {
+            const p = profile as GoogleProfile;
+            return p.email_verified && p.email.endsWith("@gmail.com")
+          }
+        }
+        return false
+      },
+    }
   }));

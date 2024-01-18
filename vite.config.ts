201c9f6d0248463a4420feaa3c28ed3a6fcee370
lieranderl <19622412+lieranderl@users.noreply.github.com>
2024-01-18 17:31:15 +0100

    close plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import ClosePlugin from './vite-plugin-close'
 
 export default defineConfig((): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), ClosePlugin()],
     optimizeDeps: {
       include: ['@auth/core'],
     },
